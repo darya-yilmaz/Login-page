8f7f1458e87518dc7bb72a9143a78a9eb660e9ea
Darya <45489622+darya-yilmaz@users.noreply.github.com>
2022-06-14 12:36:26 +0300

    Create LBTASwiftUILoginPageApp.swift

---
+++ LBTASwiftUILoginPageApp.swift/LBTASwiftUILoginPageApp.swift/LBTASwiftUILoginPage/Created by Darya Yilmaz on 13.06.2022./LBTASwiftUILoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTASwiftUILoginPageApp.swift
+//  LBTASwiftUILoginPage
+//
+//  Created by Darya Yilmaz on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct LBTASwiftUILoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
